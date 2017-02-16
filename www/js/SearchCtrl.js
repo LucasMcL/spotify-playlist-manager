bca3ad1f3c131160bb89a9a754f8e6195ce3bf89
LucasMcL <LMcLaughlin09@gmail.com>
2017-02-16 10:54:34 -0600

    makes search and logs data on form submission

--- www/js/SearchCtrl.js
+++ www/js/SearchCtrl.js
@@ -3,7 +3,21 @@ angular.module('SearchCtrl', [])
 .controller('SearchCtrl', function($scope, Spotify) {
   console.log('SearchCtrl instantiated')
 
-  Spotify.getCurrentUser().then(function (data) {
-    console.dir(data);
-  })
+  // Spotify.getCurrentUser().then(function (data) {
+  //   console.dir(data);
+  // })
+
+  $scope.trackResults = ['apple', 'banana']
+  $scope.artistResults = ['cucumber', 'durian fruit']
+
+  const SEARCH_BY = 'artist,track'
+
+  $scope.onSubmit = function(query) {
+  	// Make spotify api call here
+  	// Update $scope.trackResults and $scope.artistResults
+
+  	Spotify
+  		 .search(query, SEARCH_BY, {limit: 3})
+  		 .then(data => console.dir(data))
+  }
 })
