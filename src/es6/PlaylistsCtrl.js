a6803251694c4fc2ad6a4809d6fcbf8da3ba2e8a
LucasMcL <LMcLaughlin09@gmail.com>
2017-03-20 15:35:31 -0500

    only shows playlists owned by user

--- src/es6/PlaylistsCtrl.js
+++ src/es6/PlaylistsCtrl.js
@@ -4,13 +4,25 @@ angular.module('PlaylistsCtrl', [])
   console.log('playlist control instantiated')
 
   $scope.playlists = []
+  let userid
 
   // Checks for auth on entering this view
   // After that, loads in playlists for that user
   $scope.$on("$ionicView.enter", function() {
     Auth.verify().then(() => {
       console.log('auth has done been checked in the playlist ctrl')
-      Playlists.get().then(playlists => {$scope.playlists = playlists})
+      Spotify.getCurrentUser()
+        .then(user => {
+          userid = user.id
+          Playlists.get().then(playlists => {
+            function isOwnedByUser(playlist) {
+              return playlist.owner.id === userid
+            }
+            $scope.playlists = playlists.filter(isOwnedByUser)
+          })
+        })
     })
   })
+
+
 })
--- www/js/PlaylistsCtrl.js
+++ www/js/PlaylistsCtrl.js
@@ -4,14 +4,21 @@ angular.module('PlaylistsCtrl', []).controller('PlaylistsCtrl', function ($scope
   console.log('playlist control instantiated');
 
   $scope.playlists = [];
+  var userid = void 0;
 
   // Checks for auth on entering this view
   // After that, loads in playlists for that user
   $scope.$on("$ionicView.enter", function () {
     Auth.verify().then(function () {
       console.log('auth has done been checked in the playlist ctrl');
-      Playlists.get().then(function (playlists) {
-        $scope.playlists = playlists;
+      Spotify.getCurrentUser().then(function (user) {
+        userid = user.id;
+        Playlists.get().then(function (playlists) {
+          function isOwnedByUser(playlist) {
+            return playlist.owner.id === userid;
+          }
+          $scope.playlists = playlists.filter(isOwnedByUser);
+        });
       });
     });
   });
